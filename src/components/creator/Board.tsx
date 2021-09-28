3b5342e65c350593e73946da630c0a7ef93384d2
Anton Gunnarsson <gson.anton@hotmail.com>
2021-09-28 19:08:31 +0200

    Delete drawer

--- src/components/creator/Board.tsx
+++ src/components/creator/Board.tsx
@@ -2,7 +2,6 @@ import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import styled from 'styled-components'
 import { useActions } from '../../overmind'
-import { Drawer } from './Drawer'
 import { Segments } from './Segments'
 
 export const Board = () => {
